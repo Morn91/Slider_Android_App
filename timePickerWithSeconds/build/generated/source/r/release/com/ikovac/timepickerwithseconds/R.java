62e773e37bcaff411223457d019e534e4102bccd
Sergey Lukyanov <kyanmorn@gmail.com>
2016-04-18 00:13:47 +0300

    Some changes

--- timePickerWithSeconds/build/generated/source/r/release/com/ikovac/timepickerwithseconds/R.java
+++ timePickerWithSeconds/build/generated/source/r/release/com/ikovac/timepickerwithseconds/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static int activity_horizontal_margin=0x7f040000;
-        public static int activity_vertical_margin=0x7f040001;
+        public static int activity_horizontal_margin=0x7f050000;
+        public static int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static int ic_launcher=0x7f020000;
@@ -35,21 +35,21 @@ public final class R {
         public static int main=0x7f070000;
     }
     public static final class string {
-        public static int action_settings=0x7f050000;
-        public static int app_name=0x7f050001;
-        public static int cancel=0x7f050002;
-        public static int set=0x7f050003;
-        public static int time=0x7f050004;
-        public static int time_set=0x7f050005;
+        public static int action_settings=0x7f040000;
+        public static int app_name=0x7f040001;
+        public static int cancel=0x7f040002;
+        public static int set=0x7f040005;
+        public static int time=0x7f040003;
+        public static int time_set=0x7f040004;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
         public static int AppBaseTheme=0x7f060000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
